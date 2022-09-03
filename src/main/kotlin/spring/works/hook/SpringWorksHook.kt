a5dd77c77bd27a928adbf0fb3f1209ea02edff0e
Hannah <ghdcoalss33@gmail.com>
2022-09-03 14:31:35 +0900

    feat(ping): add ping api

--- src/main/kotlin/spring/works/hook/SpringWorksHook.kt
+++ src/main/kotlin/spring/works/hook/SpringWorksHook.kt
@@ -3,6 +3,8 @@ package spring.works.hook
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 
 @EnableScheduling
 @SpringBootApplication
@@ -10,4 +12,16 @@ class SpringWorksHook
 
 fun main(args: Array<String>) {
     runApplication<SpringWorksHook>(*args)
+}
+
+@Controller
+class SpringWorksHookController {
+
+    @GetMapping(value = ["/ping"], produces = ["application/json"])
+    fun ping(): String {
+        return buildString {
+            append("OK")
+        }
+    }
+
 }
